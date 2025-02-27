d7aaf778f00c166c97c0bdb7f94cf547a534db57
Anshuman <anshuman.raj@accropolix.live>
2025-02-27 21:25:11 +0530

    content updated

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xxl': '1568px', // Custom breakpoint at 1568px
+      },
+      spacing: {
+        '120': '120px', // Add custom padding-top 120px
+      },
+      colors: {
+        'primary': '#2B7DC2',
+        'secondary': '#003C6E',
+      },
+      backgroundImage: {
+        'custom-image': "url('/images/hero-bg.png')",
+      },
+      boxShadow: {
+        'left-right-gray': '-4px 0 10px rgba(128, 128, 128, 0.3), 4px 0 10px rgba(128, 128, 128, 0.3)',
+      },
+    },
+  },
+  plugins: [],
+}
+
